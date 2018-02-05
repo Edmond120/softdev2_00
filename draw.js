b622d30128947414c3d5bad14cb3beac3a3a7de0
edmond120 <edmondwong888@msn.com>
2018-02-05 14:41:24 -0500

    done

--- draw.js
+++ draw.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("slate");
 var ctx = canvas.getContext("2d");
 var square = false;
+var px = -1;
+var py = -1;
 canvas.addEventListener("click", function(e){
 	if(e.pageX < 100 && e.pageY < 100){
 		ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -18,11 +20,29 @@ canvas.addEventListener("click", function(e){
 		ctx.beginPath()
 		ctx.fillStyle = "green";
 		if(square){
+			if(px != -1 && py != -1){
+				ctx.moveTo(px,py);
+				ctx.lineTo(e.pageX,e.pageY);
+				ctx.stroke();
+				ctx.fill();
+				ctx.beginPath();
+			}
+			px = e.pageX;
+			py = e.pageY;
 			ctx.rect(e.pageX - 50, e.pageY - 50, 100, 100);
-			ctx.fill()
+			ctx.fill();
 			ctx.stroke();
 		}
 		else{
+			if(px != -1 && py != -1){
+				ctx.moveTo(px,py);
+				ctx.lineTo(e.pageX,e.pageY);
+				ctx.stroke();
+				ctx.fill();
+				ctx.beginPath();
+			}
+			px = e.pageX;
+			py = e.pageY;
 			ctx.arc(e.pageX,e.pageY,50,0,Math.PI * 2);
 			ctx.stroke();
 			ctx.fill();
